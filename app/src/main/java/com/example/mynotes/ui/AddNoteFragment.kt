e4afea8e1a345751de720b7c983f2f801f4267ac
Rohan <rkakar2000@gmail.com>
2019-12-08 00:19:29 +0530

    Started writing to database using async

--- app/src/main/java/com/example/mynotes/ui/AddNoteFragment.kt
+++ app/src/main/java/com/example/mynotes/ui/AddNoteFragment.kt
@@ -1,14 +1,18 @@
 package com.example.mynotes.ui
 
 
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 import com.example.mynotes.R
+import com.example.mynotes.db.Note
 import com.example.mynotes.db.NoteDatabase
+import kotlinx.android.synthetic.main.fragment_add_note.*
 
 /**
  * A simple [Fragment] subclass.
@@ -26,5 +30,40 @@ class AddNoteFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+
+        button_save.setOnClickListener{
+
+            val noteTitle = edit_text_title.toString().trim()
+            val nodeBody = edit_text_note.toString().trim()
+
+            if(noteTitle.isEmpty()){
+                edit_text_title.error = "title required"
+                edit_text_title.requestFocus()
+                return@setOnClickListener
+            }
+
+            val note = Note(noteTitle,nodeBody)
+
+            saveNote(note)
+
+
+        }
+    }
+
+    private fun saveNote(note:Note){
+        class SaveNote :AsyncTask<Void, Void, Void>(){
+            override fun doInBackground(vararg params: Void?): Void? {
+                NoteDatabase(activity!!).getNoteDao().addNode(note)
+                return null
+            }
+
+            override fun onPostExecute(result: Void?) {
+                super.onPostExecute(result)
+
+                Toast.makeText(activity, "Note Saved", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        SaveNote().execute()
     }
 }
